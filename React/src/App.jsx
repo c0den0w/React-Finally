8a03dc163a39d259f418c69ca50e87aae1ab9274
c0den0w <yupredtenteen@proton.me>
2026-02-19 00:12:53 +0530

    added my photo and servicenow logo to assets folder, plus modificaitions in App.css, App.jsx and NavBar.css files

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import NavBar from "./NavBar.jsx";
+import Hero from "./Hero.jsx";
 import './App.css'
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <NavBar />
+      <Hero />
     </>
   );
 }
