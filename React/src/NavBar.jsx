c03987a466f44f68ee8d56ea195cdc822d417c8c
c0den0w <yupredtenteen@proton.me>
2026-02-21 08:27:49 +0530

    Home link from NavBar and rearranged

--- React/src/NavBar.jsx
+++ React/src/NavBar.jsx
@@ -4,10 +4,10 @@ export default function NavBar({ onBlogsClick }) {
         <div className="navbar-container">
             <nav className="navbar">
                 <ul className="nav-links">
-                    <li><a href="/">Home</a></li>
                     <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
                     <li><a href="#" onClick={(e) => { e.preventDefault(); onBlogsClick?.(); }}>Blogs</a></li>
+                    <li><a href="/">Work</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
         </div>
